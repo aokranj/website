e116a062ef8478f5fcfa3303a8063d3c5bd54697
Bojan Hribernik <bojan.hribernik@gmail.com>
2016-03-04 23:41:04 +0100

    Fix styles and translations

--- public/wp-content/themes/aokranj/aokranj.js
+++ public/wp-content/themes/aokranj/aokranj.js
@@ -1,9 +1,9 @@
 jQuery(document).ready(function(){
 
-    // fix header 
+    // fix header
     jQuery(window).resize(function(){
         var width = jQuery(window).width();
-        if (width > 768 && width < 992) {
+        if (width >= 768 && width < 992) {
             jQuery('.header').addClass('wide-nav');
         } else {
             jQuery('.header').removeClass('wide-nav');
