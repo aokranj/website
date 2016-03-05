27e08d66460a05c8e5de557cdb68e4336192f998
Bojan Hribernik <bojan.hribernik@gmail.com>
2016-03-05 18:06:49 +0000

    Fix old path

--- public/wp-content/plugins/aokranj/config.php
+++ public/wp-content/plugins/aokranj/config.php
@@ -5,7 +5,7 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 /**
  * AO Kranj wordpress plugin settings
  */
-define('AOKRANJ_DEBUG', true);
+define('AOKRANJ_DEBUG', false);
 define('AOKRANJ_PLUGIN_VERSION', '1.0');
 define('AOKRANJ_PLUGIN_URL', rtrim(plugin_dir_url(__FILE__), '/'));
 define('AOKRANJ_PLUGIN_DIR', rtrim(plugin_dir_path(__FILE__), '/'));
@@ -18,7 +18,7 @@ define('AOKRANJ_TABLE_VZPONI', 'ao_vzponi');
 /**
  * AO Kranj old website settings
  */
-define('AOKRANJ_OLD_DIR', '/home/ao/www/www.aokranj.com/public');
+define('AOKRANJ_OLD_DIR', '/home/ao/www/old.aokranj.com/public');
 define('AOKRANJ_OLD_DB_NAME', 'aokranj_old');
 define('AOKRANJ_OLD_DB_USER', 'bojan');
 define('AOKRANJ_OLD_DB_PASSWORD', 'KladuPaKl1n');
