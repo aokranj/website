51adc885f27067e477acea3cf7a4305cf075652e
Bojan Hribernik <bojan.hribernik@gmail.com>
2016-03-06 04:45:56 +0100

    Add plugin dist config

---
+++ public/wp-content/plugins/aokranj/config.dist.php
@@ -0,0 +1,37 @@
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+/**
+ * AO Kranj wordpress plugin settings
+ */
+define('AOKRANJ_DEBUG', true);
+define('AOKRANJ_PLUGIN_VERSION', '1.0');
+define('AOKRANJ_PLUGIN_URL', rtrim(plugin_dir_url(__FILE__), '/'));
+define('AOKRANJ_PLUGIN_DIR', rtrim(plugin_dir_path(__FILE__), '/'));
+
+/**
+ * AO Kranj tables
+ */
+define('AOKRANJ_TABLE_VZPONI', 'ao_vzponi');
+
+/**
+ * AO Kranj old website settings
+ */
+define('AOKRANJ_OLD_DIR', '/path/to/old/aokranj');
+define('AOKRANJ_OLD_DB_NAME', 'aokranj_old');
+define('AOKRANJ_OLD_DB_USER', 'user');
+define('AOKRANJ_OLD_DB_PASSWORD', 'pass');
+define('AOKRANJ_OLD_DB_HOST', 'localhost');
+define('AOKRANJ_OLD_DB_CHARSET', 'utf8');
+define('AOKRANJ_OLD_DB_COLLATE', '');
+
+/**
+ * Temporary
+ */
+function dump($data, $simple = true)
+{
+    echo '<pre style="font-family:\'Lucida Grande\',\'Lucida Sans Unicode\',Trebuchet;font-size:11px;border:1px solid #606060;color:#606060;background:#fafafa;margin:10px;padding:8px;">';
+    $simple ? print_r($data) : var_dump($data);
+    echo '</pre>';
+}
