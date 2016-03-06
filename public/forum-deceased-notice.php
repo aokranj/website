620e3cd6d3a7fc9792c8d6b55d41fa079ef005eb
Bojan Hribernik <bojan.hribernik@gmail.com>
2016-03-06 00:26:04 +0000

    Add /forum deceased notice

---
+++ public/forum-deceased-notice.php
@@ -0,0 +1,20 @@
+<?php
+
+header('HTTP/1.1 404 This /forum has ceased to be');
+header('Content-Type: text/plain');
+
+?>
+Dear visitor of our /forum:
+
+Look, matey, I know a dead /forum when I see one, and I'm looking at one right now.
+It's passed on!
+This /forum is no more!
+It has ceased to be!
+It's expired and gone to meet its maker!
+It's a stiff!
+Bereft of life, it rests in peace!
+Its backend processes are now history!
+It's off the net!
+It's kicked the bucket,
+It's shuffled off its mortal coil, run down the curtain and joined the bleedin' choir invisible!!
+THIS IS AN EX-/FORUM!!!
