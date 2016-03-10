d2728f2d3156d8d9969e094140903d9e178e7812
Bojan Hribernik <bojan.hribernik@gmail.com>
2016-03-10 07:40:21 +0100

    Logo

--- public/wp-content/themes/aokranj/aokranj.js
+++ public/wp-content/themes/aokranj/aokranj.js
@@ -2,12 +2,13 @@ jQuery(document).ready(function(){
 
     // fix header
     jQuery(window).resize(function(){
-        var width = jQuery(window).width();
+        var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+        console.log('width', width);
         if (width >= 768 && width < 992) {
             jQuery('.header').addClass('wide-nav');
         } else {
             jQuery('.header').removeClass('wide-nav');
         }
     });
-    
+
 });
