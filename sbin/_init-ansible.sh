498d96bde56501a8cce5243cf21cd9f0b9a86840
Bostjan Skufca <bostjan@a2o.si>
2016-03-13 03:42:25 +0000

    ansible: do not show output from env-setup

--- sbin/_init-ansible.sh
+++ sbin/_init-ansible.sh
@@ -16,7 +16,7 @@ export PATH="/usr/local/ansible-git/bin:$PATH"
 ANSIBLE_PATH=`which ansible`
 ANSIBLE_REALPATH=`realpath $ANSIBLE_PATH`
 ANSIBLE_DIRPATH=`dirname $ANSIBLE_REALPATH`
-source $ANSIBLE_DIRPATH/../hacking/env-setup
+source $ANSIBLE_DIRPATH/../hacking/env-setup 2>&1 | tail -n+29
 
 
 
