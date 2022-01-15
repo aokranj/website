0de2e00fc84d8548868b17d0ac4d6bf1bfcefbb4
Bostjan Skufca Jese <bostjan@skufca.si>
2022-01-15 14:27:41 +0000

    unify pd-ao repos: Replace old db/ cobbled-together scripts with wp-cli-configmaps for wp_options management

--- db/lan_to_aokranj_com.sql
+++
@@ -1,4 +0,0 @@
-UPDATE wp_options SET option_value = replace(option_value, 'http://aokranj2.lan', 'http://nov.aokranj.com') WHERE option_name = 'home' OR option_name = 'siteurl';
-UPDATE wp_posts SET guid = replace(guid, 'http://aokranj2.lan', 'http://nov.aokranj.com');
-UPDATE wp_posts SET post_content = replace(post_content, 'http://aokranj2.lan', 'http://nov.aokranj.com');
-UPDATE wp_postmeta SET meta_value = replace(meta_value, 'http://aokranj2.lan', 'http://nov.aokranj.com');
--- db/schema.sql-exportDbToFile.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-
-MYDIR=`dirname $0`
-cd $MYDIR
-
-$MYDIR/../sbin/db-dump-schema > $MYDIR/schema.sql
