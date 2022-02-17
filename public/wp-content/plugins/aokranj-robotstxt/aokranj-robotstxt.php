903387935eb2262c1987a547d6c60e3f79d2ff36
Bostjan Skufca Jese <bostjan@skufca.si>
2022-02-17 15:32:15 +0000

    plugins: Remove old aokranj-robotstxt plugin (it has been superseded by aokranj-wp-misc plugin)

--- public/wp-content/plugins/aokranj-robotstxt/aokranj-robotstxt.php
+++
@@ -1,9 +0,0 @@
-<?php
-/*
-Plugin Name: aokranj-robotstxt
-Plugin URI: https://github.com/aokranj/website-aokranj.si
-Description: DEPRECATED, functionality migrated to aokranj-wp-misc
-Version: 1.0.0
-Author: Bostjan Skufca Jese
-Author URI: https://github.com/bostjan
-*/
