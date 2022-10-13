d98f77b42a3bd412b26913fcefb8651f62a28a33
Bostjan Skufca Jese <bostjan@skufca.si>
2022-10-13 00:30:20 +0000

    tooling: Refactor WordPress updating procedure

--- sbin/_init-script.sh
+++ sbin/_init-script.sh
@@ -14,7 +14,7 @@ set -u
 MYDIR_REL=`dirname "$0"`
 MYDIR_ABS=`realpath "$MYDIR_REL"`
 ROOTDIR=`dirname "$MYDIR_ABS"`
-CONFIGDIR="$ROOTDIR/config"
+CONFIGDIR="$ROOTDIR/conf"
 PUBDIR="$ROOTDIR/public"
 WPDIR="$PUBDIR"
 WORDPRESSDIR="$PUBDIR"
@@ -28,3 +28,26 @@ SBINDIR="$ROOTDIR/sbin"
 ### Change dir to project repo root
 #
 cd $ROOTDIR
+
+
+
+### Fatal error handler
+#
+_fatalError() {
+    ERR_FILE="$0"
+    ERR_MSG="$1"
+    ERR_LINE="${2:--}"
+    echo "[$ERR_FILE:$ERR_LINE] ERROR: $ERR_MSG" 1>&2
+    exit 1
+}
+
+
+
+### Message output handlers
+#
+_echo() {
+    echo "[$0] $1"
+}
+_debug() {
+    echo "[$0] [DEBUG] $1"
+}
